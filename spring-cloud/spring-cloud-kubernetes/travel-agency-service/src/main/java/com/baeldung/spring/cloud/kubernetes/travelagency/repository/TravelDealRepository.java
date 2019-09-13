5d951ef5996bc99e2a300c217ca1a56994f4993f
polakams <sarab@polakams.com>
2019-09-13 12:43:58 -0400

    Initial

--- spring-cloud/spring-cloud-kubernetes/travel-agency-service/src/main/java/com/baeldung/spring/cloud/kubernetes/travelagency/repository/TravelDealRepository.java
+++ spring-cloud/spring-cloud-kubernetes/travel-agency-service/src/main/java/com/baeldung/spring/cloud/kubernetes/travelagency/repository/TravelDealRepository.java
@@ -2,11 +2,9 @@ package com.baeldung.spring.cloud.kubernetes.travelagency.repository;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+
 import com.baeldung.spring.cloud.kubernetes.travelagency.model.TravelDeal;
-import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface TravelDealRepository extends MongoRepository<TravelDeal, String> {
-
-    public List<TravelDeal> findByDestination(String destination);
-
+public interface TravelDealRepository extends JpaRepository<TravelDeal, String> {
 }
